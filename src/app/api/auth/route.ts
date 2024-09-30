aeaf3141a28d17e902e63478e8a1b9a599b99066
Denis Ohrimenko <woxer77@gmail.com>
2024-09-30 19:53:58 +0300

    fix: deleted 'never used' NextRequest

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function POST(NextRequest) {
+export async function POST() {
   return NextResponse.json({ message: 'success' }, { status: 200 });
 }
