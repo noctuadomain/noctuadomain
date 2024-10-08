6796703d0c6c27350e27fb125292b3d263ee4cac
Denis Ohrimenko <woxer77@gmail.com>
2024-10-09 01:35:43 +0300

    feat: adaptive design for header feat: adaptive design for hero, added socials section for tablets in hero

--- src/components/elements/header.tsx
+++ src/components/elements/header.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { ClassProps } from '@/ts/interfaces';
-import { cn } from '@/lib/utils';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { ClassProps } from '@/ts/interfaces';
+import { cn } from '@/lib/utils';
 import socials from '@/data/socials';
 import sections from '@/data/sections';
 
 const Header: React.FC<ClassProps> = ({ className }) => {
   return (
-    <header className={cn('container gap-10 py-10 flex-space-between', className)} id="home">
-      <div className="flex items-center">
-        <div className="mr-44">
-          <Image src="/logo.png" alt="logo" width={232} height={40} />
+    <header
+      className={cn('container gap-10 px-4 py-10 flex-space-between lg:gap-5', className)}
+      id="home"
+    >
+      <div className="flex items-center lg:w-full lg:justify-between md:justify-center">
+        <div className="mr-44 2xl:mr-32 xl:mr-20 lg:mr-0">
+          <Image
+            className="2xl:h-[30px] 2xl:w-[174px] xl:h-[25px] xl:w-[150px] md:h-[30px] md:w-[174px]"
+            src="/logo.png"
+            alt="logo"
+            width={232}
+            height={40}
+          />
         </div>
         <nav>
-          <ul className="flex gap-8 text-lg">
-            {sections.map((section) => (
+          <ul className="flex gap-8 text-lg 2xl:gap-6 2xl:text-base xl:text-sm lg:gap-3 md:hidden">
+            {sections.map(section => (
               <li
                 className="flex-shrink-0 transition-colors hover:text-cyan hover:duration-300"
                 key={section.name}
@@ -27,8 +37,8 @@ const Header: React.FC<ClassProps> = ({ className }) => {
           </ul>
         </nav>
       </div>
-      <ul className="flex items-center gap-5 text-lg">
-        {socials.map((social) => (
+      <ul className="flex items-center gap-5 text-lg 2xl:gap-4 2xl:text-base xl:text-sm lg:hidden">
+        {socials.map(social => (
           <li
             className="opacity-50 transition-opacity hover:opacity-100 hover:duration-300"
             key={social.name}
--- src/components/elements/hero.tsx
+++ src/components/elements/hero.tsx
@@ -9,24 +9,27 @@ import Link from 'next/link';
 
 import { ClassProps } from '@/ts/interfaces';
 import { cn } from '@/lib/utils';
+import socials from '@/data/socials';
 
 const Hero: React.FC<ClassProps> = ({ className }) => {
   return (
-    <section className={cn('container relative flex-space-between', className)}>
-      <Glow className="-bottom-[400px] right-20 h-[500px] w-[320px]" />
-      <Glow className="-top-20 left-20 h-[750px] w-[550px] rotate-45 opacity-75" />
-      <div className="flex max-w-[790px] flex-col">
-        <h1 className="font-phosphate text-[155px] leading-[0.85]">ANIMATION STUDIO</h1>
-        <p className="mb-20 text-xl font-light">
+    <section className={cn('container relative px-4 flex-space-between md:flex-col', className)}>
+      <Glow className="-bottom-[400px] right-20 h-[500px] w-[320px] 3xl:h-[400px] 3xl:w-[270px] 2xl:blur-[60px] xl:-bottom-[300px] xl:right-6 xl:h-[320px] xl:w-[180px] md:-bottom-[200px] md:-right-32 md:size-[335px]" />
+      <Glow className="-top-20 left-20 h-[750px] w-[550px] rotate-45 opacity-75 3xl:h-[600px] 3xl:w-[400px] 2xl:blur-[60px] xl:-top-10 xl:h-[420px] xl:w-[320px] lg:h-[350px] lg:w-[250px] md:-left-20 md:-top-20 md:size-[400px]" />
+      <div className="flex max-w-[790px] flex-col md:order-2">
+        <h1 className="font-phosphate text-[155px] leading-[0.85] 3xl:text-[125px] 2xl:text-[116px] xl:text-[95px] lg:text-[80px] md:mb-5 md:text-center md:text-[116px]">
+          ANIMATION STUDIO
+        </h1>
+        <p className="mb-20 text-xl font-light 2xl:text-base xl:mb-14 xl:text-[14px] lg:mb-10 lg:text-[12px] lg:leading-5 md:mb-5 md:text-center md:text-base">
           All you need to do is trust our extensive experience and enjoy the journey. Share your
           thoughts with us, and we will start building your story today.
         </p>
-        <div className="flex gap-5">
+        <div className="flex gap-5 md:flex-center">
           <Link href="#contact-us" tabIndex={-1}>
             <Button className="flip-animate group px-10">
               <span data-hover="Get in touch">Contact Us</span>
               <Image
-                className="ml-6 transition-transform duration-300 group-hover:translate-x-3"
+                className="ml-6 transition-transform duration-300 group-hover:translate-x-3 xl:h-5 xl:w-7 xl:group-hover:translate-x-2"
                 src="/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={36}
@@ -37,7 +40,7 @@ const Hero: React.FC<ClassProps> = ({ className }) => {
           <Link href="#showreel" tabIndex={-1}>
             <Button className="group px-10" variant="ghost">
               <Image
-                className="mr-6 group-hover:scale-[1.15] group-hover:transition-transform group-hover:duration-300"
+                className="mr-6 group-hover:scale-[1.15] group-hover:transition-transform group-hover:duration-300 xl:size-7"
                 src="/icons/play-in-circle.svg"
                 alt="play-in-circle"
                 width={32}
@@ -47,8 +50,29 @@ const Hero: React.FC<ClassProps> = ({ className }) => {
             </Button>
           </Link>
         </div>
+        <div className="my-12 hidden gap-8 md:flex-center">
+          {socials.map(social => (
+            <Link
+              key={social.name}
+              href={social.href}
+              target="_blank"
+              aria-label="Connect with us on our social media platforms"
+            >
+              <social.icon
+                className={`box-content size-[20px] ${social.name.toLowerCase()}-white`}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
-      <Image src="/owl_halfmoon.png" alt="owl-halfmoon" width={900} height={900} priority={true} />
+      <Image
+        className="3xl:size-[775px] 2xl:size-[675px] xl:size-[575px] lg:size-[475px] md:order-1 md:size-[675px]"
+        src="/owl_halfmoon.png"
+        alt="owl-halfmoon"
+        width={900}
+        height={900}
+        priority={true}
+      />
     </section>
   );
 };
