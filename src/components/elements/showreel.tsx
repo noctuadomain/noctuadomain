62156a143e9da15431fbee8ae08f31744068330b
Denis Ohrimenko <woxer77@gmail.com>
2024-10-08 21:03:17 +0300

    refactor: deleted autoplay&loop, added controls on showreel video

--- src/components/elements/showreel.tsx
+++ src/components/elements/showreel.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+
 import { ClassProps } from '@/ts/interfaces';
 import { cn } from '@/lib/utils';
 
 const Showreel: React.FC<ClassProps> = ({ className }) => {
   return (
-    <section className={cn('container mb-44 pt-16 flex-center', className)} id="showreel">
+    <section
+      className={cn('container mb-44 pt-16 flex-center 2xl:mb-36 xl:mb-20', className)}
+      id="showreel"
+    >
       <video
         src="/showreel.mp4"
         width={1680}
         height={1105}
         muted
-        autoPlay
-        loop
+        // autoPlay
+        // loop
+        controls
         poster="/poster.webp"
       />
     </section>
