d9451e7685f4c17df9ac6c8590a8c30247d51758
Denis Ohrimenko <woxer77@gmail.com>
2024-10-10 18:18:17 +0300

    feat: added custom adaptive media queries, added adaptive design for about us section

--- src/components/elements/about-us.tsx
+++ src/components/elements/about-us.tsx
@@ -9,12 +9,12 @@ const AboutUs: React.FC<ClassProps> = ({ className }) => {
   return (
     <section
       className={cn(
-        'container relative my-44 px-20 flex-center 3xl:px-16 2xl:my-36 2xl:px-12 xl:my-20 xl:px-8',
+        'container relative my-44 px-20 flex-center 3xl:px-16 2xl:my-36 2xl:px-12 xl:my-20 xl:px-8 sm:px-4',
         className
       )}
     >
       <Glow className="-left-20 bottom-20 h-[450px] w-[300px] rotate-[45deg] 3xl:h-[350px] 3xl:w-[250px] 2xl:bottom-0 2xl:blur-[60px] xl:h-[300px] xl:w-[200px] md:hidden" />
-      <p className="text-center font-phosphate text-[96px] uppercase leading-tight 2xl:text-[72px] xl:text-[66px] md:text-[60px]">
+      <p className="about-us-text text-center font-phosphate text-[96px] uppercase leading-tight 2xl:text-[72px] xl:text-[66px] md:text-[54px] sm:text-[36px]">
         Noctua Production is a 3D
         <span className="text-gradient-orange"> animation</span> and{' '}
         <span className="text-gradient-red-to-blue">design studio</span> that transforms your ideas
