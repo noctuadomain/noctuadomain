42c9d72a42eba96827901fff12f0844c98996fcc
Denis Ohrimenko <woxer77@gmail.com>
2024-10-10 18:18:47 +0300

    feat: adaptive design for hero section

--- src/components/elements/hero.tsx
+++ src/components/elements/hero.tsx
@@ -14,13 +14,13 @@ import socials from '@/data/socials';
 const Hero: React.FC<ClassProps> = ({ className }) => {
   return (
     <section className={cn('container relative px-4 flex-space-between md:flex-col', className)}>
-      <Glow className="-bottom-[400px] right-20 h-[500px] w-[320px] 3xl:h-[400px] 3xl:w-[270px] 2xl:blur-[60px] xl:-bottom-[300px] xl:right-6 xl:h-[320px] xl:w-[180px] md:-bottom-[200px] md:-right-32 md:size-[335px]" />
-      <Glow className="-top-20 left-20 h-[750px] w-[550px] rotate-45 opacity-75 3xl:h-[600px] 3xl:w-[400px] 2xl:blur-[60px] xl:-top-10 xl:h-[420px] xl:w-[320px] lg:h-[350px] lg:w-[250px] md:-left-20 md:-top-20 md:size-[400px]" />
+      <Glow className="-bottom-[400px] right-20 h-[500px] w-[320px] 3xl:h-[400px] 3xl:w-[270px] 2xl:blur-[60px] xl:-bottom-[300px] xl:right-6 xl:h-[320px] xl:w-[180px] md:-bottom-[200px] md:-right-32 md:size-[335px] sm:hidden" />
+      <Glow className="-top-20 left-20 h-[750px] w-[550px] rotate-45 opacity-75 3xl:h-[600px] 3xl:w-[400px] 2xl:blur-[60px] xl:-top-10 xl:h-[420px] xl:w-[320px] lg:h-[350px] lg:w-[250px] md:-left-20 md:-top-20 md:size-[400px] sm:hidden" />
       <div className="flex max-w-[790px] flex-col md:order-2">
-        <h1 className="font-phosphate text-[155px] leading-[0.85] 3xl:text-[125px] 2xl:text-[116px] xl:text-[95px] lg:text-[80px] md:mb-5 md:text-center md:text-[116px]">
+        <h1 className="animation-studio font-phosphate text-[155px] leading-[0.85] 3xl:text-[125px] 2xl:text-[116px] xl:text-[95px] lg:text-[80px] md:mb-5 md:text-center md:text-[116px]">
           ANIMATION STUDIO
         </h1>
-        <p className="mb-20 text-xl font-light 2xl:text-base xl:mb-14 xl:text-[14px] lg:mb-10 lg:text-[12px] lg:leading-5 md:mb-5 md:text-center md:text-base">
+        <p className="animation-studio-desc mb-20 text-xl font-light 2xl:text-base xl:mb-14 xl:text-[14px] lg:mb-10 lg:text-[12px] lg:leading-5 md:mb-5 md:text-center md:text-base">
           All you need to do is trust our extensive experience and enjoy the journey. Share your
           thoughts with us, and we will start building your story today.
         </p>
@@ -66,7 +66,7 @@ const Hero: React.FC<ClassProps> = ({ className }) => {
         </div>
       </div>
       <Image
-        className="3xl:size-[775px] 2xl:size-[675px] xl:size-[575px] lg:size-[475px] md:order-1 md:size-[675px]"
+        className="owl-halfmoon 3xl:size-[775px] 2xl:size-[675px] xl:size-[575px] lg:size-[475px] md:order-1 md:size-[675px]"
         src="/owl_halfmoon.png"
         alt="owl-halfmoon"
         width={900}
