3412734818798fd2d062bfca53d8188783d99152
Denis Ohrimenko <woxer77@gmail.com>
2024-10-11 15:34:02 +0300

    Refactor: Adjust line height in title component

--- src/components/elements/title.tsx
+++ src/components/elements/title.tsx
@@ -17,7 +17,7 @@ const Title: React.FC<TitleProps> = ({ className, title, description, ...props }
       )}
       {...props}
     >
-      <h2 className="mb-5 text-[96px] leading-[104px] 2xl:text-[72px] 2xl:leading-[80px] lg:text-[66px] lg:leading-[72px] md:text-[60px] md:leading-[68px] sm:mb-3 sm:text-[30px] sm:leading-[34px]">
+      <h2 className="mb-5 text-[96px] leading-[110px] 2xl:text-[72px] 2xl:leading-[84px] lg:text-[66px] lg:leading-[76px] md:text-[60px] md:leading-[70px] sm:mb-3 sm:text-[30px] sm:leading-[36px]">
         {title}
       </h2>
       <h3 className="text-5xl 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-sm">{description}</h3>
