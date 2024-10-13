ac42b07d4f9105c365463947dbbdc5b9b16ffa57
Denis Ohrimenko <woxer77@gmail.com>
2024-10-13 19:12:43 +0300

    fix5: added more webkit properties and padding&margin

--- src/components/elements/about-us.tsx
+++ src/components/elements/about-us.tsx
@@ -16,22 +16,12 @@ const AboutUs: React.FC<ClassProps> = ({ className }) => {
       <Glow className="-left-20 bottom-20 h-[450px] w-[300px] rotate-[45deg] 3xl:h-[350px] 3xl:w-[250px] 2xl:bottom-0 2xl:blur-[60px] xl:h-[300px] xl:w-[200px] md:hidden" />
       <div className="about-us-text text-center font-phosphate text-8xl uppercase 2xl:text-7xl xl:text-6xl md:text-5xl sm:text-4xl">
         Noctua Production is a 3D
-        <div className="text-gradient-orange inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-14">
-          {' '}
-          animation
-        </div>{' '}
-        and{' '}
-        <div className="text-gradient-red-to-blue inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-14">
-          design studio
-        </div>{' '}
-        that transforms your ideas into stunning visual content. From concept to final video, we{' '}
-        <div className="text-gradient-blue inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-14">
-          handle
-        </div>{' '}
-        <div className="text-gradient-orange inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-14">
-          every step
-        </div>{' '}
-        with a dedicated team of professionals.
+        <div className="text-gradient-orange inline-block"> animation</div> and{' '}
+        <div className="text-gradient-red-to-blue inline-block">design studio</div> that transforms
+        your ideas into stunning visual content. From concept to final video, we{' '}
+        <div className="text-gradient-blue inline-block">handle</div>{' '}
+        <div className="text-gradient-orange inline-block">every step</div> with a dedicated team of
+        professionals.
       </div>
     </section>
   );
