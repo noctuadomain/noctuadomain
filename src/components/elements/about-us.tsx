eb4dce25910982ea2e260dea71dab9e2ec61c661
Denis Ohrimenko <woxer77@gmail.com>
2024-10-13 19:24:41 +0300

    fix#6: changed div to span, added !important, changed background to background-image

--- src/components/elements/about-us.tsx
+++ src/components/elements/about-us.tsx
@@ -16,11 +16,11 @@ const AboutUs: React.FC<ClassProps> = ({ className }) => {
       <Glow className="-left-20 bottom-20 h-[450px] w-[300px] rotate-[45deg] 3xl:h-[350px] 3xl:w-[250px] 2xl:bottom-0 2xl:blur-[60px] xl:h-[300px] xl:w-[200px] md:hidden" />
       <div className="about-us-text text-center font-phosphate text-8xl uppercase 2xl:text-7xl xl:text-6xl md:text-5xl sm:text-4xl">
         Noctua Production is a 3D
-        <div className="text-gradient-orange inline-block"> animation</div> and{' '}
-        <div className="text-gradient-red-to-blue inline-block">design studio</div> that transforms
-        your ideas into stunning visual content. From concept to final video, we{' '}
-        <div className="text-gradient-blue inline-block">handle</div>{' '}
-        <div className="text-gradient-orange inline-block">every step</div> with a dedicated team of
+        <span className="text-gradient-orange"> animation</span> and{' '}
+        <span className="text-gradient-red-to-blue">design studio</span> that transforms your ideas
+        into stunning visual content. From concept to final video, we{' '}
+        <span className="text-gradient-blue">handle</span>{' '}
+        <span className="text-gradient-orange">every step</span> with a dedicated team of
         professionals.
       </div>
     </section>
