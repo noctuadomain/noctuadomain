6ec5ff36d8be173e48a3d18183b5f7416025f846
Denis Ohrimenko <woxer77@gmail.com>
2024-10-13 19:57:14 +0300

    fix#15

--- src/components/elements/about-us.tsx
+++ src/components/elements/about-us.tsx
@@ -16,12 +16,14 @@ const AboutUs: React.FC<ClassProps> = ({ className }) => {
       <Glow className="-left-20 bottom-20 h-[450px] w-[300px] rotate-[45deg] 3xl:h-[350px] 3xl:w-[250px] 2xl:bottom-0 2xl:blur-[60px] xl:h-[300px] xl:w-[200px] md:hidden" />
       <div className="about-us-text text-center font-phosphate text-8xl uppercase 2xl:text-7xl xl:text-6xl md:text-5xl sm:text-4xl">
         Noctua Production is a 3D
-        <em className="text-gradient-orange inline-block leading-loose"> animation</em> and{' '}
-        <em className="text-gradient-red-to-blue inline-block leading-loose">design studio</em> that
+        <em className="text-gradient-orange inline-block leading-none"> animation</em> and{' '}
+        <em className="text-gradient-red-to-blue inline-block leading-tight">design studio</em> that
         transforms your ideas into stunning visual content. From concept to final video, we{' '}
-        <em className="text-gradient-blue inline-block leading-loose">handle</em>{' '}
-        <em className="text-gradient-orange inline-block leading-loose">every step</em> with a
+        <em className="text-gradient-blue inline-block leading-snug">handle</em>{' '}
+        <em className="text-gradient-orange inline-block leading-normal">every step</em> with a
         dedicated team of professionals.
+        <em className="text-gradient-orange inline-block leading-relaxed">every step2</em>
+        <em className="text-gradient-orange inline-block leading-loose">every step3</em>
       </div>
     </section>
   );
