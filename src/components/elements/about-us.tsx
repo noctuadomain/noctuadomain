c358a556f9b7386a011b49680578db345b0f4c74
Denis Ohrimenko <woxer77@gmail.com>
2024-10-13 18:58:50 +0300

    fix#3: trying to make block instead of inline-block

--- src/components/elements/about-us.tsx
+++ src/components/elements/about-us.tsx
@@ -16,19 +16,19 @@ const AboutUs: React.FC<ClassProps> = ({ className }) => {
       <Glow className="-left-20 bottom-20 h-[450px] w-[300px] rotate-[45deg] 3xl:h-[350px] 3xl:w-[250px] 2xl:bottom-0 2xl:blur-[60px] xl:h-[300px] xl:w-[200px] md:hidden" />
       <div className="about-us-text text-center font-phosphate text-8xl uppercase 2xl:text-7xl xl:text-6xl md:text-5xl sm:text-4xl">
         Noctua Production is a 3D
-        <div className="text-gradient-orange inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
+        <div className="text-gradient-orange block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
           {' '}
           animation
         </div>{' '}
         and{' '}
-        <div className="text-gradient-red-to-blue inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
+        <div className="text-gradient-red-to-blue block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
           design studio
         </div>{' '}
         that transforms your ideas into stunning visual content. From concept to final video, we{' '}
-        <div className="text-gradient-blue inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
+        <div className="text-gradient-blue block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
           handle
         </div>{' '}
-        <div className="text-gradient-orange inline-block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
+        <div className="text-gradient-orange block min-h-24 2xl:min-h-[72px] xl:min-h-[60px] md:min-h-12 sm:min-h-10">
           every step
         </div>{' '}
         with a dedicated team of professionals.
